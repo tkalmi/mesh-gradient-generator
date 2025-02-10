b32840273acc10357e3d43e7fd920ef1d90e3790
Tomi Kalmi <tomi.kalmi@gmail.com>
2025-02-10 04:12:47 +0200

    Implement new points system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
-import { Color, ColorModel, CoonsPatch, CubicBezier } from './types';
+import { Color, ColorModel, CoonsPatch, CubicBezier, Vec2 } from './types';
 import { CONTROL_POINT_RADIUS, MARGIN } from './constants';
 import {
   clamp,
@@ -23,30 +23,123 @@ import { renderCoonsPatchWithFFD } from './meshGradient/coonsPatchFFD';
 import { renderCoonsPatchWithSubdivision } from './meshGradient/coonsPatchSubdivision';
 import { renderControlPoints } from './meshGradient/controlPoints';
 
+function getNewPoints(rowCount: number, columnCount: number): Vec2[] {
+  const newPoints: Vec2[] = [];
+
+  for (let i = 0; i <= rowCount * 3; i++) {
+    for (let j = 0; j <= columnCount * 3; j++) {
+      if (i % 3 !== 0 && j % 3 !== 0) {
+        continue;
+      }
+      newPoints.push([
+        (j / (columnCount * 3)) * 100,
+        (i / (rowCount * 3)) * 100,
+      ]);
+    }
+  }
+
+  return newPoints;
+}
+
+function getColors(rowCount: number, columnCount: number): Color[] {
+  const colors: Color[] = [];
+  for (let i = 0; i < (columnCount + 1) * (rowCount + 1); i++) {
+    colors.push([
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255),
+      255,
+    ]);
+  }
+  return colors;
+}
+
+function getColumnsAndRowsFromPoints(
+  rawPoints: Vec2[],
+  columnCount: number,
+  rowCount: number
+): { columns: CubicBezier[]; rows: CubicBezier[] } {
+  const backupPoints = getNewPoints(rowCount, columnCount);
+  const points =
+    backupPoints.length === rawPoints.length ? rawPoints : backupPoints;
+  const rows: CubicBezier[] = [];
+  for (let i = 0; i <= rowCount; i++) {
+    for (let j = 0; j < columnCount; j++) {
+      const startInd = i * (columnCount * 5 + 3) + j * 3;
+      const row: CubicBezier = [
+        points[startInd],
+        points[startInd + 1],
+        points[startInd + 2],
+        points[startInd + 3],
+      ];
+      rows.push(row);
+    }
+  }
+
+  const columns: CubicBezier[] = [];
+  for (let j = 0; j < rowCount; j++) {
+    for (let i = 0; i <= columnCount; i++) {
+      const column: Vec2[] = [
+        points[j * (columnCount * 5 + 3) + (i % (columnCount + 1)) * 3],
+        points[(j + 1) * (columnCount * 3 + 1) + 2 * j * (columnCount + 1) + i],
+        points[
+          (j + 1) * (columnCount * 3 + 1) + (2 * j + 1) * (columnCount + 1) + i
+        ],
+        points[(j + 1) * (columnCount * 5 + 3) + (i % (columnCount + 1)) * 3],
+      ];
+
+      columns.push(column as CubicBezier);
+    }
+  }
+
+  return { columns, rows };
+}
+
 function getCoonsPatchFromRowsAndColumns(
   columns: CubicBezier[],
   rows: CubicBezier[],
-  colors: Color[]
+  colors: Color[],
+  columnCount: number,
+  rowCount: number
 ): CoonsPatch<Color>[] {
   const patches: CoonsPatch<Color>[] = [];
-  for (let i = 1; i < rows.length; i++) {
-    for (let j = 1; j < columns.length; j++) {
-      const north = rows[j - 1];
-      const south = rows[j].slice().reverse() as CubicBezier;
-      const west = columns[i - 1].slice().reverse() as CubicBezier;
-      const east = columns[i];
+
+  for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < columnCount; j++) {
+      // Take the ith row; this is north
+      // Take the (i + columnCount)th row and flip it; this is south
+      // Take the column with the same starting point as north's start, then flip it; this is west
+      // Take the column with the same starting point as north's end; this is east
+      const north = rows[i * columnCount + j];
+      const south = rows[(i + 1) * columnCount + j]
+        .slice()
+        .reverse() as CubicBezier;
+      const west = columns
+        .find(
+          (column) =>
+            column[0][0] === north[0][0] && column[0][1] === north[0][1]
+        )
+        ?.slice()
+        .reverse() as CubicBezier;
+      const east = columns.find(
+        (column) => column[0][0] === north[3][0] && column[0][1] === north[3][1]
+      ) as CubicBezier;
+
+      const coonsValues = {
+        northValue: colors[i * (columnCount + 1) + (j % (columnCount + 1))],
+        eastValue: colors[i * (columnCount + 1) + (j % (columnCount + 1)) + 1],
+        southValue:
+          colors[(i + 1) * (columnCount + 1) + (j % (columnCount + 1)) + 1],
+        westValue:
+          colors[(i + 1) * (columnCount + 1) + (j % (columnCount + 1))],
+      };
 
       const coonsPatch: CoonsPatch<Color> = {
         north,
         east,
         south,
         west,
-        coonsValues: {
-          northValue: colors[(j - 1) * 2],
-          eastValue: colors[(j - 1) * 2 + 1],
-          southValue: colors[j * 2 + 1],
-          westValue: colors[j * 2],
-        },
+        coonsValues,
       };
 
       patches.push(coonsPatch);
@@ -67,38 +160,10 @@ function App() {
   }>({ left: 0, top: 0, height: 600, width: 800 });
   const colorPickerRef = useRef<HTMLInputElement>(null);
 
-  const [rows, setRows] = useState<CubicBezier[]>([
-    [
-      [0, 0],
-      [33, 0],
-      [67, 0],
-      [100, 0],
-    ],
-    [
-      [0, 100],
-      [33, 100],
-      [67, 100],
-      [100, 100],
-    ],
-  ]);
-
-  const [columns, setColumns] = useState<CubicBezier[]>([
-    [
-      [0, 0],
-      [0, 33],
-      [0, 67],
-      [0, 100],
-    ],
-    [
-      [100, 0],
-      [100, 33],
-      [100, 67],
-      [100, 100],
-    ],
-  ]);
+  const [points, setPoints] = useState<Vec2[]>(getNewPoints(1, 1));
 
   // In RGBA, two for each row
-  const [colors, setColors] = useState<Color[]>([
+  const [rawColors, setColors] = useState<Color[]>([
     [255, 0, 0, 255],
     [0, 255, 0, 255],
     [255, 0, 255, 255],
@@ -112,8 +177,26 @@ function App() {
   const [colorModel, setColorModel] = useState<ColorModel>('rgba');
   const [subdivisionCount, setSubdivisionCount] = useState(5);
   const [renderContext, setRenderContext] = useState<'2d' | 'webgl'>('webgl');
+  const [rowCount, setRowCount] = useState(1);
+  const [columnCount, setColumnCount] = useState(1);
 
-  const [activeColorIndex, setActiveColorIndex] = useState<number | null>(null);
+  const { columns, rows } = getColumnsAndRowsFromPoints(
+    points,
+    columnCount,
+    rowCount
+  );
+
+  const colors = (() => {
+    const newColors = getColors(rowCount, columnCount);
+    if (newColors.length === rawColors.length) {
+      return rawColors;
+    }
+    return newColors;
+  })();
+
+  const [activeColorIndex, setActiveColorIndex] = useState<
+    [number, number] | null
+  >(null);
 
   const convertedColors = useMemo(() => {
     // Convert to proper color model here if necessary
@@ -127,6 +210,19 @@ function App() {
         return colors;
     }
   }, [colors, colorModel]);
+
+  useEffect(() => {
+    const newPoints = getNewPoints(rowCount, columnCount);
+    setPoints(newPoints);
+
+    setColors((prevColors) => {
+      const newColors = [
+        ...prevColors,
+        ...getColors(rowCount, columnCount),
+      ].slice(0, (rowCount + 1) * (columnCount + 1));
+      return newColors;
+    });
+  }, [rowCount, columnCount]);
 
   const coordinatesToPixels = useCallback(
     (patch: CoonsPatch<Color>): CoonsPatch<Color> => {
@@ -180,7 +276,9 @@ function App() {
       const patches = getCoonsPatchFromRowsAndColumns(
         columns,
         rows,
-        convertedColors
+        convertedColors,
+        columnCount,
+        rowCount
       );
 
       for (const patch of patches) {
@@ -214,7 +312,9 @@ function App() {
       const patches = getCoonsPatchFromRowsAndColumns(
         columns,
         rows,
-        convertedColors
+        convertedColors,
+        columnCount,
+        rowCount
       );
       for (const patch of patches) {
         const coonsPatch = coordinatesToPixels(patch);
@@ -249,6 +349,8 @@ function App() {
   }, [
     columns,
     rows,
+    columnCount,
+    rowCount,
     patchType,
     rasterizerAlgorithm,
     colorModel,
@@ -272,18 +374,14 @@ function App() {
   }, []);
 
   const lastMouseDownTimestampRef = useRef(0);
-  const draggedPointRowAndColumnIndexRef = useRef<
-    [[number, number] | null, [number, number] | null]
-  >([null, null]);
+  const draggedPointIndexRef = useRef<number | null>(null);
 
-  const getHoveredColumnAndRowPointIndexes = useCallback(
-    (
-      event: React.MouseEvent<HTMLElement>
-    ): [[number, number] | null, [number, number] | null] => {
+  const getHoveredPointIndex = useCallback(
+    (event: React.MouseEvent<HTMLElement>): number | null => {
       const { left, top, width, height } = canvasDimensionsRef.current;
       const x = event.clientX - left;
       const y = event.clientY - top;
-      const columnPoint = columns.flat().findIndex((point) => {
+      const index = points.findIndex((point) => {
         const [px, py] = point;
         return (
           Math.abs(
@@ -294,25 +392,10 @@ function App() {
           ) <= CONTROL_POINT_RADIUS
         );
       });
-      const rowPoint = rows.flat().findIndex((point) => {
-        const [px, py] = point;
-        return (
-          Math.abs(convertXToCanvasX(px, width) - x) <= CONTROL_POINT_RADIUS &&
-          Math.abs(convertYToCanvasY(py, height) - y) <= CONTROL_POINT_RADIUS
-        );
-      });
-      const columnPointIndex =
-        columnPoint > -1
-          ? ([Math.floor(columnPoint / 4), columnPoint % 4] as [number, number])
-          : null;
-      const rowPointIndex =
-        rowPoint > -1
-          ? ([Math.floor(rowPoint / 4), rowPoint % 4] as [number, number])
-          : null;
 
-      return [columnPointIndex, rowPointIndex];
+      return index ?? null;
     },
-    [columns, rows]
+    [points]
   );
 
   const handleMouseDown = useCallback(
@@ -326,28 +409,16 @@ function App() {
         setActiveColorIndex(null);
       }
 
-      const [columnPointIndex, rowPointIndex] =
-        getHoveredColumnAndRowPointIndexes(event);
-
-      draggedPointRowAndColumnIndexRef.current = [
-        columnPointIndex,
-        rowPointIndex,
-      ];
+      draggedPointIndexRef.current = getHoveredPointIndex(event);
     },
-    [getHoveredColumnAndRowPointIndexes, activeColorIndex]
+    [getHoveredPointIndex, activeColorIndex]
   );
 
   const handleMouseMove = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
       const container = containerRef.current as HTMLDivElement;
-      if (
-        draggedPointRowAndColumnIndexRef.current.every(
-          (index) => index === null
-        )
-      ) {
-        if (
-          getHoveredColumnAndRowPointIndexes(event).some((ind) => ind != null)
-        ) {
+      if (draggedPointIndexRef.current === null) {
+        if (getHoveredPointIndex(event) !== null) {
           container.style.cursor = 'grab';
         } else {
           container.style.cursor = 'default';
@@ -374,36 +445,51 @@ function App() {
           (height - MARGIN.top - MARGIN.bottom)) *
         100;
 
-      const [columnIndex, rowIndex] = draggedPointRowAndColumnIndexRef.current;
-      if (columnIndex !== null) {
-        setColumns((columns) => {
-          const clone = columns.slice();
-          clone[columnIndex[0]][columnIndex[1]] = [x, y];
-          return clone;
-        });
-      }
-      if (rowIndex !== null) {
-        setRows((rows) => {
-          const clone = rows.slice();
-          clone[rowIndex[0]][rowIndex[1]] = [x, y];
-          return clone;
-        });
-      }
+      setPoints((prevPoints) => {
+        const nextPoints = [...prevPoints];
+        nextPoints[draggedPointIndexRef.current!] = [x, y];
+        return nextPoints;
+      });
     },
-    [getHoveredColumnAndRowPointIndexes]
+    [getHoveredPointIndex]
   );
 
   const handleMouseUp = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
-      draggedPointRowAndColumnIndexRef.current = [null, null];
-      const hoveredColumnAndRowPointIndexes =
-        getHoveredColumnAndRowPointIndexes(event);
+      function getControlPointColorIndex(
+        pointIndex: number | null
+      ): number | null {
+        if (pointIndex === null) {
+          return null;
+        }
+        const fullRowLength = columnCount * 3 + 1;
+        const controlRowLength = columnCount + 1;
+        let index = pointIndex;
+        let rowsAdded = 0;
+        while (true) {
+          if (index < 0) {
+            return null;
+          } else if (index < fullRowLength) {
+            return index % 3 === 0
+              ? index / 3 + rowsAdded * (columnCount + 1)
+              : null;
+          } else if (index >= fullRowLength + 2 * controlRowLength) {
+            index -= fullRowLength + 2 * controlRowLength;
+            rowsAdded++;
+          } else {
+            return null;
+          }
+        }
+      }
+
+      draggedPointIndexRef.current = null;
+      const hoveredPointIndex = getHoveredPointIndex(event);
       // Detect if the mouse was dragged or clicked by comparing
       if (performance.now() - lastMouseDownTimestampRef.current < 200) {
         // If it was clicked on top of a corner control point, open color palette
-        const rowPointIndex = hoveredColumnAndRowPointIndexes[1];
-        if (rowPointIndex != null && [0, 3].includes(rowPointIndex[1])) {
-          setActiveColorIndex(rowPointIndex[0] * 2 + (rowPointIndex[1] % 2));
+        const colorIndex = getControlPointColorIndex(hoveredPointIndex);
+        if (hoveredPointIndex !== null && colorIndex !== null) {
+          setActiveColorIndex([colorIndex, hoveredPointIndex]);
           setTimeout(() => colorPickerRef.current?.click(), 50); // Set a minor delay to give the color picker time to position right
         } else {
           setActiveColorIndex(null);
@@ -413,17 +499,17 @@ function App() {
       }
 
       const container = containerRef.current as HTMLDivElement;
-      if (hoveredColumnAndRowPointIndexes.some((ind) => ind != null)) {
+      if (hoveredPointIndex !== null) {
         container.style.cursor = 'grab';
       } else {
         container.style.cursor = 'default';
       }
     },
-    [getHoveredColumnAndRowPointIndexes]
+    [getHoveredPointIndex, columnCount]
   );
 
   const maxSubdivisions = 8; /* 
-  Theoretically the maximum depth should be something like this, but it's pretty heavy, so limit it to 8, which is the upper limit for my MBP.
+  Theoretically the maximum depth should be something like this, but it's pretty heavy, so limit it to 8, which seems to be the upper limit for my MBP.
   Math.round(
     Math.max(
       Math.log2(canvasDimensionsRef.current.width / (columns.length - 1)),
@@ -580,6 +666,42 @@ function App() {
               WebGL
             </label>
           </fieldset>
+
+          <fieldset>
+            <legend>Select patch count</legend>
+            <label>
+              <input
+                style={{ marginInline: '0.5em' }}
+                id="row-count"
+                value={rowCount}
+                onChange={(event) =>
+                  setRowCount(
+                    clamp(1, 4, Math.round(Number(event.target.value ?? 0)))
+                  )
+                }
+                type="number"
+                min={1}
+                max={4}
+              />
+              rows
+            </label>
+            <label>
+              <input
+                style={{ marginInline: '0.5em' }}
+                id="column-count"
+                value={columnCount}
+                onChange={(event) =>
+                  setColumnCount(
+                    clamp(1, 4, Math.round(Number(event.target.value ?? 0)))
+                  )
+                }
+                type="number"
+                min={1}
+                max={4}
+              />
+              columns
+            </label>
+          </fieldset>
         </form>
 
         <div style={{ width: 800, height: 600, position: 'relative' }}>
@@ -591,28 +713,24 @@ function App() {
               height: 0,
               position: 'absolute',
               top: convertYToCanvasY(
-                rows[Math.floor((activeColorIndex ?? 0) / 2)][
-                  (activeColorIndex ?? 0) % 2 ? 3 : 0
-                ][1],
+                points[activeColorIndex?.[1] ?? 0][1],
                 canvasRef.current?.height ?? 0
               ),
               left: convertXToCanvasX(
-                rows[Math.floor((activeColorIndex ?? 0) / 2)][
-                  (activeColorIndex ?? 0) % 2 ? 3 : 0
-                ][0],
+                points[activeColorIndex?.[1] ?? 0][0],
                 canvasRef.current?.width ?? 0
               ),
               pointerEvents: activeColorIndex == null ? 'none' : 'auto',
             }}
             type="color"
             id="color-picker"
-            value={rgbaToHex(colors[activeColorIndex ?? 0])}
+            value={rgbaToHex(colors[activeColorIndex?.[0] ?? 0])}
             autoFocus
             onChange={(event: React.FormEvent<HTMLInputElement>) => {
               const value = event.currentTarget.value;
               setColors((prevColors) =>
                 prevColors.map((color, ind) =>
-                  ind === activeColorIndex ? hexToRgb(value) : color
+                  ind === activeColorIndex?.[0] ? hexToRgb(value) : color
                 )
               );
             }}
